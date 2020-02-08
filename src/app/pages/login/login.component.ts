38abd7e3715b86d3c0715e8b96ccf84a24210a76
Manoth Klangphila <manothklangphila@Manoths-MacBook-Pro.local>
2020-02-08 14:25:13 +0700

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵɵresolveBody } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    document.body.className = 'hold-transition login-page';
   }
 
 }
